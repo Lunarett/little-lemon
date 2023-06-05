849e9bdf3ab28363a86b29707c58d6e4a102c478
Egor Ageev <54112318+Lunarett@users.noreply.github.com>
2023-06-05 12:26:03 +0200

    Setup Basic Page Components

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './Logo.svg';
+
+function Header() {
+    return (
+        <header>
+            <img src={logo} />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './Logo.svg'
+
+function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li><a>Home</a></li>
+                <li><a>About</a></li>
+                <li><a>Reservations</a></li>
+                <li><a>Order Online</a></li>
+                <li><a>Login</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
